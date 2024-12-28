9394083c00b8a30b8d33d17c08af3238bcac478f
Nasir Masud <nasir.masud@ymail.com>
2024-12-28 23:08:59 +0600

    First Commit of this project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export const metadata = {
+  title: "Home",
+};
+
+const HomePage = () => {
+  return <Button>NextEcommerce</Button>;
+};
+
+export default HomePage;
---
+++ lib/constants/index.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "NextEcommerce";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "A Cool ecommerce app made by NextJS";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "https://localhost:3000";
