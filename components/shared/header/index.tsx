accccd064fb35fe84365bfbdb07f37efed7cb1a9
Nasir Masud <nasir.masud@ymail.com>
2024-12-31 19:21:27 +0600

    Added the sheet component for small screen menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,7 @@
-import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
-import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 const Header = () => {
   return (
@@ -24,17 +22,7 @@ const Header = () => {
           </Link>
         </div>
         <div className='space-x-2'>
-          <ModeToggle />
-          <Button asChild variant='ghost'>
-            <Link href='/cart'>
-              <ShoppingCart size={24} /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href='/sign-in'>
-              <UserIcon size={24} /> Sign In
-            </Link>
-          </Button>
+          <Menu />
         </div>
       </div>
     </header>
