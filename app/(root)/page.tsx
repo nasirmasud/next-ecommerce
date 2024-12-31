813ee0f45a7e47a41a16e894456c74bf39899900
Nasir Masud <nasir.masud@ymail.com>
2024-12-31 20:37:31 +0600

    Added card component for products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import ProductList from "@/components/shared/product/product-list";
 import sampleData from "@/db/sample-data";
 
 const HomePage = () => {
-  console.log(sampleData);
-
-  return <>NextEcommerce</>;
+  return (
+    <>
+      <ProductList data={sampleData.products} title='Featured Products' />
+    </>
+  );
 };
 
 export default HomePage;
