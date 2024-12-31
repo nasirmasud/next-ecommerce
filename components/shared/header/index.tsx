c02210c7d93b84cbf6e41dc7bde8649ed6d19cdc
Nasir Masud <nasir.masud@ymail.com>
2024-12-31 11:49:28 +0600

    Added the appearance for page with dark mode

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import { APP_NAME } from "@/lib/constants";
 import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -23,6 +24,7 @@ const Header = () => {
           </Link>
         </div>
         <div className='space-x-2'>
+          <ModeToggle />
           <Button asChild variant='ghost'>
             <Link href='/cart'>
               <ShoppingCart size={24} /> Cart
