a4315b60af22343f46e66ec3fee4ba4b910887c8
Nasir Masud <nasir.masud@ymail.com>
2025-01-03 19:48:42 +0600

    Added serverless environment

--- lib/actions/product-actions.ts
+++ lib/actions/product-actions.ts
@@ -1,13 +1,11 @@
 "use server";
 
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 import { prismaToJsObject } from "../utils";
 
 //get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
