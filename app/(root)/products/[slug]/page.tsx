6fd059d283d9c8e0c1f4fd6c4afc0d5a0a62a981
Nasir Masud <nasir.masud@ymail.com>
2025-01-04 20:35:51 +0600

    added product image component

--- app/(root)/products/[slug]/page.tsx
+++ app/(root)/products/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import ProductImages from "@/components/shared/product/product-images";
 import ProductPrice from "@/components/shared/product/product-pice";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
@@ -17,7 +18,9 @@ const ProductDetailsPage = async (props: {
       <section>
         <div className='grid grid-cols-1 md:grid-cols-5'>
           {/* Images Column */}
-          <div className='col-span-2'>{/* Image component */}</div>
+          <div className='col-span-2'>
+            <ProductImages images={product.images} />
+          </div>
           {/* Details column */}
           <div className='col-span-2 p-5'>
             <div className='flex flex-col gap-6'>
---
+++ components/shared/product/product-images.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useState } from "react";
+
+const ProductImages = ({ images }: { images: string[] }) => {
+  const [current, setCurrent] = useState(0);
+
+  return (
+    <div className='space-y-4'>
+      <Image
+        src={images[current]}
+        alt='product image'
+        width={700}
+        height={700}
+        className='min-h-[300px] object-center object-cover'
+      />
+      <div className='flex'>
+        {images.map((image, index) => (
+          <div
+            key={image}
+            onClick={() => setCurrent(index)}
+            className={cn(
+              "border mr2 cursor-pointer hover:border-orange-800",
+              current === index && "border-orange-400"
+            )}
+          >
+            <Image src={image} alt='image' width={100} height={100} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
