622db73f0e62d8005a5bc6f46a0c99cd7aab6f89
Nasir Masud <nasir.masud@ymail.com>
2025-01-07 23:15:05 +0600

    Added UserButton component for user authentication and profile management

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -6,9 +6,10 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
+import { EllipsisVertical, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import ModeToggle from "./mode-toggle";
+import UserButton from "./user-button";
 
 const Menu = () => {
   return (
@@ -20,11 +21,7 @@ const Menu = () => {
             <ShoppingCart size={24} /> Cart
           </Link>
         </Button>
-        <Button asChild>
-          <Link href='/sign-in'>
-            <UserIcon size={24} /> Sign In
-          </Link>
-        </Button>
+        <UserButton />
       </nav>
       <nav className='md:hidden'>
         <Sheet>
@@ -39,11 +36,7 @@ const Menu = () => {
                 <ShoppingCart size={24} /> Cart
               </Link>
             </Button>
-            <Button asChild>
-              <Link href='/sign-in'>
-                <UserIcon size={24} /> Sign In
-              </Link>
-            </Button>
+            <UserButton />
             <SheetDescription></SheetDescription>
           </SheetContent>
         </Sheet>
