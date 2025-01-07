22c9ebff63419fd2caf117bcf65749d058eb7ac8
Nasir Masud <nasir.masud@ymail.com>
2025-01-07 14:25:47 +0600

    Refactor sign-in form to handle credentials submission and improve user feedback

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -3,12 +3,35 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
+
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending} className='w-full' variant='default'>
+        {pending ? "Signing In..." : "Sign In"}
+      </Button>
+    );
+  };
+
   return (
-    <form>
+    <form action={action}>
+      <input type='hidden' name='callbackUrl' value={callbackUrl} />
       <div className='space-y-6'>
         <div>
           <Label htmlFor='email'>Email</Label>
@@ -16,8 +39,8 @@ const CredentialsSignInForm = () => {
             id='email'
             name='email'
             type='email'
-            autoComplete='email'
             required
+            autoComplete='email'
             defaultValue={signInDefaultValues.email}
           />
         </div>
@@ -27,18 +50,21 @@ const CredentialsSignInForm = () => {
             id='password'
             name='password'
             type='password'
-            autoComplete='password'
             required
+            autoComplete='password'
             defaultValue={signInDefaultValues.password}
           />
         </div>
         <div>
-          <Button className='w-full' variant='default'>
-            Sign In
-          </Button>
+          <SignInButton />
         </div>
+
+        {data && !data.success && (
+          <div className='text-center text-destructive'>{data.message}</div>
+        )}
+
         <div className='text-sm text-center text-muted-foreground'>
-          Don't have an account ?{" "}
+          Don't have an account?{" "}
           <Link href='/sign-up' target='_self' className='link'>
             Sign Up
           </Link>
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,3 +1,4 @@
+import { auth } from "@/auth";
 import {
   Card,
   CardContent,
@@ -6,27 +7,45 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { APP_NAME } from "@/lib/constants";
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import CredentialsSignInForm from "./credentials-signin-form";
 
-const SignIn = () => {
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+const SignInPage = async (props: {
+  searchParams: Promise<{
+    callbackUrl: string;
+  }>;
+}) => {
+  const { callbackUrl } = await props.searchParams;
+
+  const session = await auth();
+
+  if (session) {
+    return redirect(callbackUrl || "/");
+  }
+
   return (
-    <div className='w-full max-w-md max-auto'>
+    <div className='w-full max-w-md mx-auto'>
       <Card>
         <CardHeader className='space-y-4'>
           <Link href='/' className='flex-center'>
             <Image
-              alt={`${APP_NAME} logo`}
-              priority={true}
               src='/images/logo.svg'
               width={100}
               height={100}
+              alt={`${APP_NAME} logo`}
+              priority={true}
             />
           </Link>
           <CardTitle className='text-center'>Sign In</CardTitle>
           <CardDescription className='text-center'>
-            Select a method to Sign In to your account
+            Sign in to your account
           </CardDescription>
         </CardHeader>
         <CardContent className='space-y-4'>
@@ -37,4 +56,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default SignInPage;
