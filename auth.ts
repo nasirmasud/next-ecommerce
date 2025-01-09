261d3f98886c966bab9fa1b010445cf88ce30129
Nasir Masud <nasir.masud@ymail.com>
2025-01-09 23:24:49 +0600

    Enhanced session and JWT handling to include user role and name, with fallback for missing names

--- auth.ts
+++ auth.ts
@@ -53,11 +53,32 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       //set the userId from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
+
+      console.log(token);
+
       //if there as an update, set the user name
       if (trigger === "update") {
         session.user.name = user.name;
       }
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      //Assign user fields to token
+      if (user) {
+        token.role = user.role;
+        //if user have no name, use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+          //Update database to reflect the token name
+          await prisma.user.update({
+            where: { id: user.id },
+            data: { name: token.name },
+          });
+        }
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
