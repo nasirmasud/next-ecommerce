b78d2488fe7f112c765b64798573312473db0159
Nasir Masud <nasir.masud@ymail.com>
2025-01-18 17:44:28 +0600

    Enhanced addItemToCart function with cart retrieval and item validation; implement getMyCart function for user cart access

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,9 +1,11 @@
 "use server";
 
 import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
 import { CartItem } from "@/types";
 import { cookies } from "next/headers";
-import { formatError } from "../utils";
+import { formatError, prismaToJsObject } from "../utils";
+import { cartItemSchema } from "../validitors";
 
 export async function addItemToCart(data: CartItem) {
   try {
@@ -15,10 +17,22 @@ export async function addItemToCart(data: CartItem) {
     const session = await auth();
     const userId = session?.user?.id ? (session.user.id as string) : undefined;
 
+    //Get cart
+    const cart = await getMyCart();
+    //parse and validate item
+    const item = cartItemSchema.parse(data);
+
+    //Find product in database
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+
     //Testing
     console.log({
       "Session Cart Id": sessionCartId,
       "User Id": userId,
+      "Item requested": item,
+      "Product Found": product,
     });
     return {
       success: true,
@@ -30,4 +44,28 @@ export async function addItemToCart(data: CartItem) {
       message: formatError(error),
     };
   }
+}
+
+export async function getMyCart() {
+  //Check for the cart cookie
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  if (!sessionCartId) throw new Error("Cart session not found");
+  //Get session & user id
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  //Get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+  if (!cart) return undefined;
+  //Convert and return
+  return prismaToJsObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
