36db306d4e957bfb73dd91cfa654b49f62e8b3c5
Nasir Masud <nasir.masud@ymail.com>
2025-01-19 23:47:16 +0600

    Updated default sign-in values, upgraded Prisma dependencies, and enhanced addItemToCart function with session handling and error management

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -5,47 +5,54 @@ import { ToastAction } from "@/components/ui/toast";
 import { useToast } from "@/hooks/use-toast";
 import { addItemToCart, removeFromCart } from "@/lib/actions/cart.actions";
 import { Cart, CartItem } from "@/types";
-import { Minus, Plus } from "lucide-react";
+import { Loader, Minus, Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
   const { toast } = useToast();
 
+  const [isPending, startTransition] = useTransition();
+
   //Handle add item tom cart function
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
-    if (!res.success) {
+    startTransition(async () => {
+      const res = await addItemToCart(item);
+      if (!res.success) {
+        toast({
+          variant: "destructive",
+          description: res.message,
+        });
+        return;
+      }
+      //handle success add to cart
       toast({
-        variant: "destructive",
         description: res.message,
+        action: (
+          <ToastAction
+            className='bg-primary text-white hover:bg-gray-800'
+            altText='Go To cart'
+            onClick={() => router.push("/cart")}
+          >
+            Go To Cart
+          </ToastAction>
+        ),
       });
-      return;
-    }
-    //handle success add to cart
-    toast({
-      description: res.message,
-      action: (
-        <ToastAction
-          className='bg-primary text-white hover:bg-gray-800'
-          altText='Go To cart'
-          onClick={() => router.push("/cart")}
-        >
-          Go To Cart
-        </ToastAction>
-      ),
     });
   };
 
   //Handle remove from cart function
   const handleRemoveCart = async () => {
-    const res = await removeFromCart(item.productId);
-    toast({
-      variant: res.success ? "default" : "destructive",
-      description: res.message,
-    });
+    startTransition(async () => {
+      const res = await removeFromCart(item.productId);
+      toast({
+        variant: res.success ? "default" : "destructive",
+        description: res.message,
+      });
 
-    return;
+      return;
+    });
   };
   // Check if item is in cart
   const existItem =
@@ -54,16 +61,28 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type='button' variant='outline' onClick={handleRemoveCart}>
-        <Minus className='h-4 w-4' />
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Minus className='h-4 w-4' />
+        )}
       </Button>
       <span className='px-4'>{existItem.qty}</span>
       <Button type='button' variant='outline' onClick={handleAddToCart}>
-        <Plus className='h-4 w-4' />
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Plus className='h-4 w-4' />
+        )}
       </Button>
     </div>
   ) : (
     <Button className='w-full' type='button' onClick={handleAddToCart}>
-      <Plus />
+      {isPending ? (
+        <Loader className='w-4 h-4 animate-spin' />
+      ) : (
+        <Plus className='h-4 w-4' />
+      )}
       Add To Cart
     </Button>
   );
