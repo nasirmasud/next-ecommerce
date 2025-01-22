8058ef824c61357fc0d9ccee5b8aff5644f71490
Nasir Masud <nasir.masud@ymail.com>
2025-01-22 23:18:41 +0600

    Added form components and integrate react-hook-form for shipping address

--- app/(root)/shipping-address/shipping-address-form.tsx
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { useToast } from "@/hooks/use-toast";
 import { ShippingAddress } from "@/types";
+import { useRouter } from "next/navigation";
 
 const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
   return <>Shipping Address From</>;
 };
 
