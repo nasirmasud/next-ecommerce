72bb0966be23c0f1aa51765f3a394d75996f6211
Nasir Masud <nasir.masud@ymail.com>
2025-01-26 14:58:28 +0600

    Added updateUserPaymentMethod function to manage user payment method updates

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,8 +5,10 @@ import { prisma } from "@/db/prisma";
 import { ShippingAddress } from "@/types";
 import { hashSync } from "bcrypt-ts-edge";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { z } from "zod";
 import { formatError } from "../utils";
 import {
+  paymentMethodSchema,
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
@@ -96,6 +98,29 @@ export async function updateUserAddress(data: ShippingAddress) {
       data: { address: address },
     });
     return { success: true, message: "Address updated successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+//Update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+    if (!currentUser) throw new Error("User not found");
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
+    return { success: true, message: "Payment method updated successfully" };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
