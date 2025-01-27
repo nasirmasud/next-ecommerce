a4c45192089f2569a8a0c8a776d7da9afaa5fcfd
Nasir Masud <nasir.masud@ymail.com>
2025-01-27 12:36:02 +0600

    Refactor product details page structure and routing

--- db/seed.ts
+++ db/seed.ts
@@ -5,9 +5,6 @@ async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
   await prisma.user.deleteMany();
-  // await prisma.account.deleteMany();
-  // await prisma.session.deleteMany();
-  // await prisma.verificationToken.deleteMany();
 
   await prisma.product.createMany({ data: sampleData.products });
   await prisma.user.createMany({ data: sampleData.users });
