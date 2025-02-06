987b9d86190047691aa4355063769d6bde698438
Nasir Masud <nasir.masud@ymail.com>
2025-02-06 11:40:38 +0600

    Enhance user button dropdown with profile and orders links

--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -42,26 +42,36 @@ const UserButton = async () => {
           </div>
         </DropdownMenuTrigger>
         <DropdownMenuContent className='w-56' align='end' forceMount>
-          <div className='flex flex-col space-y-1'>
-            <div className='text-sm font-medium leading-none'>
-              {session.user?.name}
+          <DropdownMenuLabel className='font-normal'>
+            <div className='flex flex-col space-y-1'>
+              <div className='text-sm font-medium leading-none'>
+                {session.user?.name}
+              </div>
+              <div className='text-sm text-muted-foreground leading-none'>
+                {session.user?.email}
+              </div>
             </div>
-            <div className='text-sm text-muted-foreground leading-none'>
-              {session.user?.email}
-            </div>
-          </div>
-          <DropdownMenuLabel>
-            <DropdownMenuItem className='p-0 mb-1'>
-              <form action={signOutUser} className='w-full'>
-                <Button
-                  className='w-full py-4 px-2 h-4 justify-start'
-                  variant='ghost'
-                >
-                  Sign Out
-                </Button>
-              </form>
-            </DropdownMenuItem>
           </DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Link href='/user/profile' className='w-full'>
+              Profile
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href='/user/orders' className='w-full'>
+              Orders
+            </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem className='p-0 mb-1'>
+            <form action={signOutUser} className='w-full'>
+              <Button
+                className='w-full py-4 px-2 h-4 justify-start'
+                variant='ghost'
+              >
+                Sign Out
+              </Button>
+            </form>
+          </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
