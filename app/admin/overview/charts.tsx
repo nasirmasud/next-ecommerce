d610803d09d48ecb895e5fa7f34cd8e2b724df02
Nasir Masud <nasir.masud@ymail.com>
2025-02-07 21:00:00 +0600

    Added Charts component for sales data visualization in admin overview

---
+++ app/admin/overview/charts.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ResponsiveContainer } from "recharts";
+
+const Charts = ({
+  data: { salesData },
+}: {
+  data: { salesData: { month: string; totalSales: number }[] };
+}) => {
+  return <ResponsiveContainer>Charts</ResponsiveContainer>;
+};
+
+export default Charts;
