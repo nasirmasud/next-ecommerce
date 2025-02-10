01564bb098f3ce2e5bb4f2d8e9ff95bb8d819c42
Nasir Masud <nasir.masud@ymail.com>
2025-02-10 17:57:12 +0600

    Added DeleteDialog component to ProductsPage for product deletion functionality

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,4 @@
+import DeleteDialog from "@/components/shared/delete-dialog";
 import Pagination from "@/components/shared/pagination";
 import { Button } from "@/components/ui/button";
 import {
@@ -8,7 +9,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { getAllProducts } from "@/lib/actions/product-actions";
+import { deleteProduct, getAllProducts } from "@/lib/actions/product-actions";
 import { formatCurrency, shortenId } from "@/lib/utils";
 import Link from "next/link";
 
@@ -64,7 +65,7 @@ const ProductsPage = async (props: {
                 <Button asChild variant='outline' size='sm'>
                   <Link href={`/admin/products/${product.id}`}>Edit</Link>
                 </Button>
-                {/*DELETE*/}
+                <DeleteDialog id={product.id} action={deleteProduct} />
               </TableCell>
             </TableRow>
           ))}
