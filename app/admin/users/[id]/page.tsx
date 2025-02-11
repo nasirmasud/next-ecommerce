66e59b0f08aa0ec0496d69abfdb565beb866e043
Nasir Masud <nasir.masud@ymail.com>
2025-02-11 23:01:03 +0600

    Added UpdateUserForm component to AdminUserUpdate for user update functionality

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { getUserById } from "@/lib/actions/user.actions";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
+import UpdateUserForm from "./update-user-form";
 
 export const metadata: Metadata = {
   title: "Update User",
@@ -14,6 +15,7 @@ const AdminUserUpdate = async (props: { params: Promise<{ id: string }> }) => {
   return (
     <div className='space-y-8 max-w-lg mx-auto'>
       <h1 className='h2-bold'>Update User</h1>
+      <UpdateUserForm user={user} />
     </div>
   );
 };
