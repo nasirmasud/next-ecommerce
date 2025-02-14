e640c2907d6059b70d103a0acf5000f88885cc2e
Nasir Masud <nasir.masud@ymail.com>
2025-02-14 16:26:11 +0600

    Replaced search input with AdminSearch component in AdminLayout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,5 @@
+import AdminSearch from "@/components/admin/admin-search";
 import Menu from "@/components/shared/header/menu";
-import { Input } from "@/components/ui/input";
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
@@ -25,11 +25,7 @@ export default function AdminLayout({
             </Link>
             <MainNav className='mx-6' />
             <div className='ml-auto items-center flex space-x-4'>
-              <Input
-                type='search'
-                placeholder='Search...'
-                className='md:w-[180px] lg:w-[300px]'
-              />
+              <AdminSearch />
               <Menu />
             </div>
           </div>
