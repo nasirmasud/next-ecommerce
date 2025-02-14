f92d3de5e800105c65f5317c2a2d75bf13356d55
Nasir Masud <nasir.masud@ymail.com>
2025-02-14 22:34:10 +0600

    Added query filtering to getAllProducts for improved product search

--- lib/actions/product-actions.ts
+++ lib/actions/product-actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { prisma } from "@/db/prisma";
+import { Prisma } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
@@ -38,14 +39,24 @@ export async function getAllProducts({
   query,
   limit = PAGE_SIZE,
   page,
-  category,
 }: {
   query: string;
   limit: number;
   page: number;
   category?: string;
 }) {
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.product.findMany({
+    where: { ...queryFilter },
     orderBy: { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
