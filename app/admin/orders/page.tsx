49428ebc1f909f3d9908a0a123c78cff73866905
Nasir Masud <nasir.masud@ymail.com>
2025-02-14 23:16:55 +0600

    Enhanced OrdersPage to support search filtering and display active filters

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -20,9 +20,9 @@ export const metadata: Metadata = {
 };
 
 const OrdersPage = async (props: {
-  searchParams: Promise<{ page: string }>;
+  searchParams: Promise<{ page: string; query: string }>;
 }) => {
-  const { page = "1" } = await props.searchParams;
+  const { page = "1", query: searchText } = await props.searchParams;
   const session = await auth();
   if (session?.user?.role !== "admin")
     throw new Error("User is not authorized");
@@ -30,16 +30,29 @@ const OrdersPage = async (props: {
   const orders = await getAllOrders({
     page: Number(page),
     limit: 6,
+    query: searchText,
   });
 
   return (
     <div className='space-y-2'>
-      <h2 className='h2-bold'>Orders</h2>
+      <div className='flex items-center gap-3'>
+        <h2 className='h2-bold'>Orders</h2>
+        {searchText && (
+          <div>
+            Filtered by <i>&quot;{searchText}&quot;</i>{" "}
+            <Link href='/admin/orders'></Link>
+            <Button variant='outline' size='sm'>
+              Remove Filter
+            </Button>
+          </div>
+        )}
+      </div>
       <Table>
         <TableHeader>
           <TableRow>
             <TableHead>ID</TableHead>
             <TableHead>DATE</TableHead>
+            <TableHead>BUYER</TableHead>
             <TableHead>TOTAL</TableHead>
             <TableHead>PAID</TableHead>
             <TableHead>DELIVERED</TableHead>
@@ -51,6 +64,7 @@ const OrdersPage = async (props: {
             <TableRow key={order.id}>
               <TableCell>{shortenId(order.id)}</TableCell>
               <TableCell>{formatDateTime(order.createdAt).dateTime}</TableCell>
+              <TableCell>{order.user.name}</TableCell>
               <TableCell>{formatCurrency(order.totalPrice)}</TableCell>
               <TableCell>
                 {order.isPaid && order.paidAt
