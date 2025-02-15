58e19226b8e78ba9b4c642d0fb47156b0f505ef3
Nasir Masud <nasir.masud@ymail.com>
2025-02-15 12:23:18 +0600

    Added query filtering to getAllUsers for improved user retrieval

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,6 +3,7 @@
 import { auth, signIn, signOut } from "@/auth";
 import { prisma } from "@/db/prisma";
 import { ShippingAddress } from "@/types";
+import { Prisma } from "@prisma/client";
 import { hashSync } from "bcrypt-ts-edge";
 import { revalidatePath } from "next/cache";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
@@ -156,11 +157,24 @@ export async function updateProfile(user: { name: string; email: string }) {
 export async function getAllUsers({
   limit = PAGE_SIZE,
   page,
+  query,
 }: {
   limit?: number;
   page: number;
+  query: string;
 }) {
+  const queryFilter: Prisma.UserWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.user.findMany({
+    where: { ...queryFilter },
     orderBy: { createdAt: "desc" },
     take: limit,
     skip: (page - 1) * limit,
