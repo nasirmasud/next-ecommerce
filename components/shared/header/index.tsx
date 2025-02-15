a0de427bd0b8a7dd01637fa3da0e7c4b1e8e5393
Nasir Masud <nasir.masud@ymail.com>
2025-02-15 15:40:00 +0600

    Added CategoryDrawer component to Header for improved navigation

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,6 +1,7 @@
 import { APP_NAME } from "@/lib/constants";
 import Image from "next/image";
 import Link from "next/link";
+import CategoryDrawer from "./category-header";
 import Menu from "./menu";
 
 const Header = () => {
@@ -8,7 +9,8 @@ const Header = () => {
     <header className='w-full border-b'>
       <div className='wrapper flex-between'>
         <div className='flex-start'>
-          <Link href='/' className='flex-start'>
+          <CategoryDrawer />
+          <Link href='/' className='flex-start ml-4'>
             <Image
               src='/images/logo.svg'
               alt={`${APP_NAME} Logo`}
