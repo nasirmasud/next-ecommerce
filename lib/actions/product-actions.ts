4fe5cbd952a417c5f39c531446866b618dd495b3
Nasir Masud <nasir.masud@ymail.com>
2025-02-15 15:15:45 +0600

    Added function to retrieve all product categories with count

--- lib/actions/product-actions.ts
+++ lib/actions/product-actions.ts
@@ -131,3 +131,12 @@ export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
     return { success: false, message: formatError(error) };
   }
 }
+
+//Get all category
+export async function getAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ["category"],
+    _count: true,
+  });
+  return data;
+}
