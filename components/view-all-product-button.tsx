7cb4b907ea3a005ef34d8b803fbfbb89425770eb
Nasir Masud <nasir.masud@ymail.com>
2025-02-16 12:39:04 +0600

    Enhanced View All Products button with bold text for improved visibility

--- components/view-all-product-button.tsx
+++ components/view-all-product-button.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 const ViewAllProductButton = () => {
   return (
     <div className='flex justify-center items-center my-8'>
-      <Button asChild className='px-8 py-4 text-lg'>
+      <Button asChild className='px-8 py-4 text-lg font-semibold'>
         <Link href='/search'>View All Products</Link>
       </Button>
     </div>
