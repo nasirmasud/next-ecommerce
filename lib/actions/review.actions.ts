4a08790dbafc855757cdcacc680e6eebe1652f0d
Nasir Masud <nasir.masud@ymail.com>
2025-03-01 18:30:44 +0600

    Added getReviews action to retrieve all reviews for a product

--- lib/actions/review.actions.ts
+++ lib/actions/review.actions.ts
@@ -70,3 +70,14 @@ export async function createUpdateReview(
     return { success: false, message: formatError(error) };
   }
 }
+
+//Get all reviews for a product
+export async function getReviews({ productId }: { productId: string }) {
+  const data = await prisma.review.findMany({
+    where: { productId: productId },
+    include: { user: { select: { name: true } } },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return { data };
+}
