ce99930e669739b4657504716e42eb2789f02b2c
Nasir Masud <nasir.masud@ymail.com>
2025-03-03 12:50:31 +0600

    Implemented Stripe payment intent creation and user authorization check in OrderDetailsPage

--- app/(root)/order/[id]/page.tsx
+++ app/(root)/order/[id]/page.tsx
@@ -2,42 +2,56 @@ import { auth } from "@/auth";
 import { getOrderById } from "@/lib/actions/order.actions";
 import { ShippingAddress } from "@/types";
 import { Metadata } from "next";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
+import Stripe from "stripe";
 import OrderDetailsTable from "./order-details-table";
 
 export const metadata: Metadata = {
   title: "Order Details",
-  description: "Order Details",
 };
 
-const OrderDetailsPage = async (props: {
-  params: Promise<{
-    id: string;
-  }>;
-}) => {
+const OrderDetailsPage = async (props: { params: Promise<{ id: string }> }) => {
   const { id } = await props.params;
   const order = await getOrderById(id);
-  if (!order || !order.paymentResult) notFound();
+  if (!order) notFound();
 
   const session = await auth();
+  // Redirect the user if they don't own the order
+  if (order.userId !== session?.user.id && session?.user.role !== "admin") {
+    return redirect("/unauthorized");
+  }
+
+  let client_secret = null;
+
+  // Check if is not paid and using stripe
+  if (order.paymentMethod === "Stripe" && !order.isPaid) {
+    // Init stripe instance
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+    // Create payment intent
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(Number(order.totalPrice) * 100),
+      currency: "USD",
+      metadata: { orderId: order.id },
+    });
+    client_secret = paymentIntent.client_secret;
+  }
 
   return (
-    <>
-      <OrderDetailsTable
-        order={{
-          ...order,
-          shippingAddress: order.shippingAddress as ShippingAddress,
-          paymentResult: order.paymentResult as {
-            id: string;
-            status: string;
-            email_address: string;
-            pricePaid: string;
-          },
-        }}
-        paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
-        isAdmin={session?.user?.role === "admin" || false}
-      />
-    </>
+    <OrderDetailsTable
+      order={{
+        ...order,
+        shippingAddress: order.shippingAddress as ShippingAddress,
+        paymentResult: order.paymentResult as {
+          id: string;
+          status: string;
+          email_address: string;
+          pricePaid: string;
+        },
+      }}
+      stripeClientSecret={client_secret}
+      paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+      isAdmin={session?.user?.role === "admin" || false}
+    />
   );
 };
 
