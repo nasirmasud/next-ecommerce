cbf8eddf6c823c921793f208b2434dd42b4bc6a3
Nasir Masud <nasir.masud@ymail.com>
2026-01-13 14:21:15 +0600

    Refactor middleware to export default auth and config
    Export default auth and add config for middleware.

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,8 @@
 export { auth as middleware } from "@/auth";
+import { auth } from "@/auth"
+
+export default auth;
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)"],
+};
